aa651aa14eed92fb6ecbbbeea8fb8a2b7b0d9d95
Kasia <s15804@pjwstk.edu.pl>
2019-10-07 23:17:46 +0200

    Implemented Game class

--- src/main/java/com/hawliczek/Game.java
+++
@@ -1,6 +0,0 @@
-package com.hawliczek;
-
-public class Game
-{
-
-}
---
+++ src/main/java/com/hawliczek/domain/Game.java
@@ -0,0 +1,81 @@
+package com.hawliczek.domain;
+
+public class Game
+{
+	private int id;
+	private String title;
+	private String genre;
+	private String developer;
+	private String publisher;
+	private String releaseDate;
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+	
+	public String getGenre()
+	{
+		return genre;
+	}
+	
+	public void setGenre(String genre)
+	{
+		this.genre = genre;
+	}
+	
+	public String getDeveloper()
+	{
+		return developer;
+	}
+	
+	public void setDeveloper(String developer)
+	{
+		this.developer = developer;
+	}
+	
+	public String getPublisher()
+	{
+		return publisher;
+	}
+	
+	public void setPublisher(String publisher)
+	{
+		this.publisher = publisher;
+	}
+	
+	public String getReleaseDate()
+	{
+		return releaseDate;
+	}
+	
+	public void setReleaseDate(String releaseDate)
+	{
+		this.releaseDate = releaseDate;
+	}
+	
+	public Game(int id, String title, String genre, String developer, String publisher, String releaseDate)
+	{
+		this.id = id;
+		this.title = title;
+		this.genre = genre;
+		this.developer = developer;
+		this.publisher = publisher;
+		this.releaseDate = releaseDate;
+	}
+}
