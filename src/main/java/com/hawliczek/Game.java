a05cbd4072bc9a296c2ad8e5472e6b3d3b798413
Kasia <s15804@pjwstk.edu.pl>
2019-10-07 19:14:36 +0200

    First testing

---
+++ src/main/java/com/hawliczek/Game.java
@@ -0,0 +1,6 @@
+package com.hawliczek;
+
+public class Game
+{
+
+}
---
+++ src/test/java/com/hawliczek/GameTest.java
@@ -0,0 +1,15 @@
+package com.hawliczek;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class GameTest
+{
+	@Test
+	public void emptyTest()
+	{
+		Assert.assertTrue(true);
+	}
+}
